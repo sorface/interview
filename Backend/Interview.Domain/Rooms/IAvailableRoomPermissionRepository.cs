906d612afb7a8c8552fd2eedd4a8a30cae360465
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2024-03-02 19:45:02 +0300

    Added AvailableRoomPermissionRepository

---
+++ Backend/Interview.Domain/Rooms/IAvailableRoomPermissionRepository.cs
@@ -0,0 +1,7 @@
+﻿using Interview.Domain.Repository;
+
+namespace Interview.Domain.Rooms;
+
+public interface IAvailableRoomPermissionRepository : IRepository<AvailableRoomPermission>
+{
+}
---
+++ Backend/Interview.Infrastructure/Rooms/AvailableRoomPermissionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Interview.Domain.Rooms;
+using Interview.Infrastructure.Database;
+
+namespace Interview.Infrastructure.Rooms;
+
+public class AvailableRoomPermissionRepository : EfRepository<AvailableRoomPermission>, IAvailableRoomPermissionRepository
+{
+    public AvailableRoomPermissionRepository(AppDbContext db)
+        : base(db)
+    {
+    }
+}
