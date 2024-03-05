003052bbafcfd8b583be5ece89233a1dde77b209
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2024-03-05 17:35:08 +0300

    Backend, Tests: Added SecurityServiceTest

--- Backend/Interview.Domain/Users/ICurrentUserAccessor.cs
+++ Backend/Interview.Domain/Users/ICurrentUserAccessor.cs
@@ -34,6 +34,16 @@ public sealed class CurrentUserAccessor : IEditableCurrentUserAccessor
 {
     private User? _currentUser;
 
+    public CurrentUserAccessor()
+        : this(null)
+    {
+    }
+
+    public CurrentUserAccessor(User? currentUser)
+    {
+        _currentUser = currentUser;
+    }
+
     public Guid? UserId => _currentUser?.Id;
 
     public User? UserDetailed => _currentUser;
