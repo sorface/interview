dfb22fbee9e11c16a62d4e5447e7fcf6ba38a9e2
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2024-03-12 20:32:59 +0300

    Backend, Tests: Fixed build

--- Backend/Interview.Test/FakerFactory.cs
+++ Backend/Interview.Test/FakerFactory.cs
@@ -9,7 +9,7 @@ namespace Interview.Test
         public Faker<Room> Room()
         {
             return new Faker<Room>()
-                .CustomInstantiator(e => new Room(string.Empty, string.Empty))
+                .CustomInstantiator(e => new Room(string.Empty, string.Empty, SERoomAcсessType.Public))
                 .RuleFor(e => e.Name, f => f.Random.Word())
                 .RuleFor(e => e.Id, f => Guid.NewGuid());
         }
