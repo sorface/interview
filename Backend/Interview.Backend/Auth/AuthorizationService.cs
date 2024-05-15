a699c4e120c59dddcca5b324a2186d7a5d832519
Павел Талайко <devpavdeveloper@yandex.ru>
2024-05-16 00:30:19 +0300

    feature/#28 - reformat code

--- Backend/Interview.Backend/Auth/AuthorizationService.cs
+++ Backend/Interview.Backend/Auth/AuthorizationService.cs
@@ -10,6 +10,7 @@ public class AuthorizationService
 
     public string ClaimsIssuer { get; set; } = string.Empty;
 
+    public string Domain { get; set; } = string.Empty;
     public PathString CallbackPath { get; set; } = PathString.Empty;
 
     public List<string> AvailableLoginRedirects { get; set; } = new();
