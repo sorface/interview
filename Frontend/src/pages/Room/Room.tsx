fa2c74b9481616fd3c99a64792d2da8be3cde785
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-05-26 13:08:17 +0300

    Frontend: Participant cursors in code editor

--- Frontend/src/pages/Room/Room.tsx
+++ Frontend/src/pages/Room/Room.tsx
@@ -170,6 +170,7 @@ export const Room: FunctionComponent = () => {
   const eventsState = useEventsState({ roomState, lastWsMessage: lastMessage });
   const codeEditorEnabled = !!eventsState[EventName.CodeEditor];
   const codeEditorLanguage = String(eventsState[EventName.CodeEditorLanguage]);
+  const codeEditorRemoteCursor = String(eventsState[EventName.CodeEditorCursor]);
 
   const currentUserExpert = roomParticipant?.userType === 'Expert';
   const currentUserExaminee = roomParticipant?.userType === 'Examinee';
@@ -506,6 +507,7 @@ export const Room: FunctionComponent = () => {
                 messagesChatEnabled={messagesChatEnabled}
                 codeEditorEnabled={codeEditorEnabled}
                 codeEditorLanguage={codeEditorLanguage}
+                codeEditorRemoteCursor={codeEditorRemoteCursor}
                 userVideoStream={userVideoStream}
                 userAudioStream={userAudioStream}
                 screenStream={screenStream}
