e0fcc03c5a5f4c250f8c2c77c19da6c9dee708ce
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-07-14 16:43:38 +0300

    Frontend: New question item for questions page

--- Frontend/src/pages/Questions/Questions.tsx
+++ Frontend/src/pages/Questions/Questions.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent, useCallback, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { GetQuestionsParams, questionsApiDeclaration } from '../../apiDeclarations';
-import { Field } from '../../components/FieldsBlock/Field';
 import { HeaderWithLink } from '../../components/HeaderWithLink/HeaderWithLink';
 import { MainContentWrapper } from '../../components/MainContentWrapper/MainContentWrapper';
 import { pathnames } from '../../constants';
@@ -9,10 +8,11 @@ import { useApiMethod } from '../../hooks/useApiMethod';
 import { Question } from '../../types/question';
 import { ProcessWrapper } from '../../components/ProcessWrapper/ProcessWrapper';
 import { QustionsSearch } from '../../components/QustionsSearch/QustionsSearch';
-import { ActionModal } from '../../components/ActionModal/ActionModal';
 import { LocalizationKey } from '../../localization';
 import { useLocalizationCaptions } from '../../hooks/useLocalizationCaptions';
 import { ItemsGrid } from '../../components/ItemsGrid/ItemsGrid';
+import { QuestionItem } from '../../components/QuestionItem/QuestionItem';
+import { ContextMenu } from '../../components/ContextMenu/ContextMenu';
 
 import './Questions.css';
 
@@ -21,6 +21,7 @@ const initialPageNumber = 1;
 
 export const Questions: FunctionComponent = () => {
   const localizationCaptions = useLocalizationCaptions();
+  const navigate = useNavigate();
   const [pageNumber, setPageNumber] = useState(initialPageNumber);
   const [searchValue, setSearchValue] = useState('');
   const { category } = useParams();
@@ -45,28 +46,35 @@ export const Questions: FunctionComponent = () => {
     setPageNumber(pageNumber + 1);
   }, [pageNumber]);
 
-  const createQuestionItem = useCallback((question: Question) => (
-    <li key={question.id}>
-      <Field className="question-item">
-        <span>{question.value}</span>
-        <div className="question-controls">
-        <Link to={pathnames.questionsEdit.replace(':id', question.id)}>
-          <button>
-            🖊️
-          </button>
-        </Link>
-        <ActionModal
-          openButtonCaption='📁'
-          error={archiveError}
-          loading={archiveLoading}
-          title={localizationCaptions[LocalizationKey.Archive]}
-          loadingCaption={localizationCaptions[LocalizationKey.ArchiveLoading]}
-          onAction={() => {archiveQuestion(question.id)}}
-        />
-        </div>
-      </Field>
+  const handleEditQuestion = (question: Question) => () => {
+    navigate(pathnames.questionsEdit.replace(':id', question.id));
+  };
+
+  const handlearchiveQuestion = (question: Question) => () => {
+    archiveQuestion(question.id);
+  };
+
+  const createQuestionItem = (question: Question) => (
+    <li key={question.id} className='pb-0.25'>
+      <QuestionItem
+        question={question}
+        primary
+        contextMenu={{
+          position: 'right',
+          children: [
+            <ContextMenu.Item
+              title={localizationCaptions[LocalizationKey.Edit]}
+              onClick={handleEditQuestion(question)}
+            />,
+            <ContextMenu.Item
+              title={archiveLoading ? localizationCaptions[LocalizationKey.ArchiveLoading] : localizationCaptions[LocalizationKey.Archive]}
+              onClick={handlearchiveQuestion(question)}
+            />,
+          ],
+        }}
+      />
     </li>
-  ), [archiveLoading, archiveError, localizationCaptions, archiveQuestion]);
+  );
 
   return (
     <MainContentWrapper className="questions-page">
@@ -79,15 +87,15 @@ export const Questions: FunctionComponent = () => {
         <QustionsSearch
           onSearchChange={setSearchValue}
         />
-        </HeaderWithLink>
+      </HeaderWithLink>
       <ProcessWrapper
         loading={false}
-        error={error|| archiveError}
+        error={error || archiveError}
       >
         <ItemsGrid
           currentData={questions}
           loading={loading}
-          triggerResetAccumData={`${searchValue}${category}`}
+          triggerResetAccumData={`${searchValue}${category}${archivedQuestion}`}
           loaderClassName='question-item field-wrap'
           renderItem={createQuestionItem}
           nextPageAvailable={questions?.length === pageSize}
