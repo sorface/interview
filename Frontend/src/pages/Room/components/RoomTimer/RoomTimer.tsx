c3c29d955d373c4395bf271a4fc94f819b3c7bb9
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-07-29 15:25:04 +0300

    Fix/#154 timer doesnt update at start (#168)
    * Frontend: Fix timer doesnt update at start
    
    * Frontend: Seconds in room timer

--- Frontend/src/pages/Room/components/RoomTimer/RoomTimer.tsx
+++ Frontend/src/pages/Room/components/RoomTimer/RoomTimer.tsx
@@ -4,16 +4,15 @@ import { ThemedIcon } from '../ThemedIcon/ThemedIcon';
 import { IconNames } from '../../../../constants';
 import { Gap } from '../../../../components/Gap/Gap';
 
-import './RoomTimer.css';
-
-const updateIntervalMs = 33 * 1000;
+const updateIntervalMs = 1000;
 
 const padTime = (value: number) => String(value).padStart(2, '0');
 
 const formatTime = (timeMs: number) => {
   const hours = Math.floor((timeMs / (1000 * 60 * 60)));
   const minutes = Math.floor((timeMs / 1000 / 60) % 60);
-  return `${padTime(hours)}:${padTime(minutes)}`;
+  const seconds = Math.floor((timeMs / 1000) % 60);
+  return `${padTime(hours)}:${padTime(minutes)}:${padTime(seconds)}`;
 };
 
 export const RoomTimer: FunctionComponent<Required<Room['timer']>> = ({
@@ -32,6 +31,7 @@ export const RoomTimer: FunctionComponent<Required<Room['timer']>> = ({
     const updateTimer = () => {
       setRemainingTimeMs(endDate.getTime() - Date.now());
     };
+    updateTimer();
     const intervalId = setInterval(updateTimer, updateIntervalMs);
 
     return () => {
@@ -40,10 +40,12 @@ export const RoomTimer: FunctionComponent<Required<Room['timer']>> = ({
   }, [startTime, durationSec]);
 
   return (
-    <div className="room-timer flex items-center bg-wrap rounded-2">
+    <div className="flex items-center justify-center px-1 py-0.625 bg-wrap rounded-2">
       <ThemedIcon name={IconNames.Time} />
       <Gap sizeRem={0.25} horizontal />
-      {formatTime(remainingTimeMs <= 0 ? 0 : remainingTimeMs)}
+      <div className='w-4 text-left'>
+        {formatTime(remainingTimeMs <= 0 ? 0 : remainingTimeMs)}
+      </div>
     </div>
   );
 };
