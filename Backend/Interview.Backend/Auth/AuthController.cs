4f355cadcf1a2f7638ae4946a3eae1e13e010a57
Павел Талайко <devpavdeveloper@yandex.ru>
2024-08-08 18:40:21 +0300

    - init session

--- Backend/Interview.Backend/Auth/AuthController.cs
+++ Backend/Interview.Backend/Auth/AuthController.cs
@@ -1,7 +1,5 @@
 using Interview.Backend.Auth.Sorface;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Interview.Backend.Auth;
