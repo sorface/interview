f26b974efb12ac378efe9867ccc286cd644cb22b
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-08-15 22:28:33 +0300

    Frontend: Fix incorrect text and icon in room question switch button

--- Frontend/src/pages/Room/components/RoomQuestionPanel/RoomQuestionPanel.tsx
+++ Frontend/src/pages/Room/components/RoomQuestionPanel/RoomQuestionPanel.tsx
@@ -222,19 +222,28 @@ export const RoomQuestionPanel: FunctionComponent<RoomQuestionPanelProps> = ({
             </div>
             <Gap sizeRem={1.8125} />
             <Button
-              className='w-full'
+              className='w-full flex items-center'
               variant='active'
               onClick={openQuestionsIds.length !== 0 ? handleNextQuestion : handleStartReviewRoom}
             >
-              {!initialQuestion ?
-                localizationCaptions[LocalizationKey.StartRoom] :
-                localizationCaptions[
-                openQuestionsIds.length !== 0 ?
-                  LocalizationKey.NextRoomQuestion :
-                  LocalizationKey.StartReviewRoom
-                ]
-              }
-              <Icon name={openQuestionsIds.length !== 0 ? IconNames.ChevronForward : IconNames.Stop} />
+              {roomQuestionsLoading || loadingRoomActiveQuestion || loadingRoomStartReview ? (
+                <Loader />
+              ) : (
+                <>
+                  <span>
+                    {!initialQuestion ?
+                      localizationCaptions[LocalizationKey.StartRoom] :
+                      localizationCaptions[
+                      openQuestionsIds.length !== 0 ?
+                        LocalizationKey.NextRoomQuestion :
+                        LocalizationKey.StartReviewRoom
+                      ]
+                    }
+                  </span>
+                  <Gap sizeRem={0.5} horizontal />
+                  <Icon name={openQuestionsIds.length !== 0 ? IconNames.ChevronForward : IconNames.Stop} />
+                </>
+              )}
             </Button>
           </div>
         </>
