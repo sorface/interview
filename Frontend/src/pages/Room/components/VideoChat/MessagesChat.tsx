03d68bc1cd8cc62ae9e9bcdd1f19bea4edfac56b
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-08-28 22:57:09 +0300

    Frontend: New messages chat appearance (#323)

--- Frontend/src/pages/Room/components/VideoChat/MessagesChat.tsx
+++ Frontend/src/pages/Room/components/VideoChat/MessagesChat.tsx
@@ -1,11 +1,15 @@
-import { FunctionComponent, useRef, KeyboardEvent, useEffect, useContext } from 'react';
+import { FunctionComponent, useRef, KeyboardEvent, useEffect, Fragment } from 'react';
 import { Transcript } from '../../../../types/transcript';
-import { stringToColor } from './utils/stringToColor';
-import { ThemeContext } from '../../../../context/ThemeContext';
+import { Theme } from '../../../../context/ThemeContext';
 import { LocalizationKey } from '../../../../localization';
 import { useLocalizationCaptions } from '../../../../hooks/useLocalizationCaptions';
 import { Button } from '../../../../components/Button/Button';
 import { Gap } from '../../../../components/Gap/Gap';
+import { useThemeClassName } from '../../../../hooks/useThemeClassName';
+import { Typography } from '../../../../components/Typography/Typography';
+import { padTime } from '../../../../utils/padTime';
+import { IconNames } from '../../../../constants';
+import { Icon } from '../Icon/Icon';
 
 import './MessagesChat.css';
 
@@ -14,12 +18,25 @@ interface MessagesChatProps {
   onMessageSubmit: (message: string) => void;
 }
 
+const formatTime = (value: Date) => {
+  const hours = padTime(value.getHours());
+  const minutes = padTime(value.getMinutes());
+  return `${hours}:${minutes}`;
+};
+
 export const MessagesChat: FunctionComponent<MessagesChatProps> = ({
   textMessages,
   onMessageSubmit,
 }) => {
   const localizationCaptions = useLocalizationCaptions();
-  const { themeInUi } = useContext(ThemeContext);
+  const messageClassName = useThemeClassName({
+    [Theme.Dark]: 'bg-dark-history-hover',
+    [Theme.Light]: 'bg-grey1',
+  });
+  const chatButtonVariant = useThemeClassName({
+    [Theme.Dark]: 'inverted' as const,
+    [Theme.Light]: 'invertedActive' as const,
+  });
   const messageInputRef = useRef<HTMLInputElement>(null);
   const videochatTranscriptsRef = useRef<HTMLDivElement>(null);
 
@@ -58,31 +75,37 @@ export const MessagesChat: FunctionComponent<MessagesChatProps> = ({
       <Gap sizeRem={0.75} />
       <div className='videochat-transcripts px-0.75' ref={videochatTranscriptsRef}>
         {textMessages.map(transcript => (
-          <div
-            key={transcript.frontendId}
-          >
-            <span
-              style={{ color: stringToColor(transcript.userNickname, themeInUi) }}
+          <Fragment key={transcript.frontendId}>
+            <div
+              className={`${messageClassName} flex flex-col py-0.25 px-0.5 rounded-0.5`}
             >
-              {transcript.userNickname}
-            </span>
-            {': '}
-            {transcript.value}
-          </div>
+              <Typography size='s' bold>{transcript.userNickname}</Typography>
+              <Gap sizeRem={0.25} />
+              <Typography size='s'>{transcript.value}</Typography>
+              <div className='text-right'>
+                <Typography size='xs' secondary>{formatTime(new Date(transcript.createdAt))}</Typography>
+              </div>
+            </div>
+            <Gap sizeRem={0.25} />
+          </Fragment>
         ))}
       </div>
+      <Gap sizeRem={0.5} />
       <div className='flex justify-between px-0.75'>
         <input
           type='text'
           placeholder={localizationCaptions[LocalizationKey.ChatMessagePlaceholder]}
           ref={messageInputRef}
-          className='w-7.5'
+          className='flex-1'
           onKeyDown={handleInputKeyDown}
         />
+        <Gap sizeRem={0.25} horizontal />
         <Button
-          className='min-w-fit !transition'
-          onClick={handleChatMessageSubmit}>
-          {localizationCaptions[LocalizationKey.SendToChat]}
+          variant={chatButtonVariant}
+          className='min-w-fit !transition rounded-full p-0 w-2.125 h-2.125 min-h-unset'
+          onClick={handleChatMessageSubmit}
+        >
+          <Icon size='s' name={IconNames.PaperPlane} />
         </Button>
       </div>
       <Gap sizeRem={0.75} />
