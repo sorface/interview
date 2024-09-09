d498f184ce8b5a4aef799e2f57c87e6db76191aa
Pavel Talaika <devpavdeveloper@yandex.ru>
2024-09-09 21:46:19 +0300

    Feature/fix migration (#378)
    * added migrations
    
    * feature/fix-migration
    - Add migration sql request
    
    * feature/fix-migration
    - Reformat code
    
    ---------
    
    Co-authored-by: Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>

--- Backend/Interview.Migrations.Sqlite/Migrations/20240909181621_room_review_rename_columns.Designer.cs
+++ Backend/Interview.Migrations.Sqlite/Migrations/20240909181621_room_review_rename_columns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Interview.Domain.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Interview.Migrations.Sqlite.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909181621_room_review_rename_columns")]
+    partial class room_review_rename_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.20");
