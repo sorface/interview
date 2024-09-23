18760957b5d40e5210723793384d84d00022a37a
Павел Талайко <devpavdeveloper@yandex.ru>
2024-09-23 08:37:43 +0300

    bugfix/#432 - Reformat code

--- Backend/Interview.Backend/Auth/Sorface/SorfaceTokenService.cs
+++ Backend/Interview.Backend/Auth/Sorface/SorfaceTokenService.cs
@@ -118,18 +118,13 @@ public class SorfaceTokenService
 
 public class RefreshTokenObject
 {
-    [JsonPropertyName("access_token")]
-    public string? AccessToken { get; set; }
+    [JsonPropertyName("access_token")] public string? AccessToken { get; set; }
 
-    [JsonPropertyName("refresh_token")]
-    public string? RefreshToken { get; set; }
+    [JsonPropertyName("refresh_token")] public string? RefreshToken { get; set; }
 
-    [JsonPropertyName("scope")]
-    public string? Scope { get; set; }
+    [JsonPropertyName("scope")] public string? Scope { get; set; }
 
-    [JsonPropertyName("token_type")]
-    public string? TokenType { get; set; }
+    [JsonPropertyName("token_type")] public string? TokenType { get; set; }
 
-    [JsonPropertyName("expires_in")]
-    public double? ExpiresIn { get; set; }
+    [JsonPropertyName("expires_in")] public double? ExpiresIn { get; set; }
 }
