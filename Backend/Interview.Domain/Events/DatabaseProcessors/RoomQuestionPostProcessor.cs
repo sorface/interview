6627d65b36aca442d4f311a6effa973b741bf386
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2024-12-04 20:08:32 +0300

    Backend, ChangingCodeEditor: Removed RoomQuestionPreProcessor

--- Backend/Interview.Domain/Events/DatabaseProcessors/RoomQuestionPostProcessor.cs
+++ Backend/Interview.Domain/Events/DatabaseProcessors/RoomQuestionPostProcessor.cs
@@ -15,17 +15,20 @@ public class RoomQuestionPostProcessor : EntityPostProcessor<RoomQuestion>
     private readonly AppDbContext _db;
     private readonly RoomCodeEditorChangeEventHandler _roomCodeEditorChangeEventHandler;
     private readonly ICurrentUserAccessor _currentUserAccessor;
+    private readonly IRoomConfigurationRepository _roomConfigurationRepository;
 
     public RoomQuestionPostProcessor(
         IRoomEventDispatcher eventDispatcher,
         AppDbContext db,
         RoomCodeEditorChangeEventHandler roomCodeEditorChangeEventHandler,
-        ICurrentUserAccessor currentUserAccessor)
+        ICurrentUserAccessor currentUserAccessor,
+        IRoomConfigurationRepository roomConfigurationRepository)
     {
         _eventDispatcher = eventDispatcher;
         _db = db;
         _roomCodeEditorChangeEventHandler = roomCodeEditorChangeEventHandler;
         _currentUserAccessor = currentUserAccessor;
+        _roomConfigurationRepository = roomConfigurationRepository;
     }
 
     public override async ValueTask ProcessAddedAsync(RoomQuestion entity, CancellationToken cancellationToken)
@@ -54,9 +57,61 @@ public class RoomQuestionPostProcessor : EntityPostProcessor<RoomQuestion>
 
         if (current.State == RoomQuestionState.Active)
         {
-            var codeEditorPayload = await GetCodeEditorEventPayload(current, cancellationToken);
-            await _roomCodeEditorChangeEventHandler.HandleAsync(current.RoomId, codeEditorPayload.Enabled, EVRoomCodeEditorChangeSource.System, cancellationToken);
+            await ChangeCodeEditorEnabledStateAsync(current, cancellationToken);
+
+            // Changed active question
+            if (original.State != RoomQuestionState.Active)
+            {
+                await ChangeCodeEditorContentAsync(current, cancellationToken);
+            }
+
+            await _db.SaveChangesAsync(cancellationToken);
         }
+    }
+
+    private async Task ChangeCodeEditorEnabledStateAsync(RoomQuestion current, CancellationToken cancellationToken)
+    {
+        var codeEditorPayload = await GetCodeEditorEventPayload(current, cancellationToken);
+        var request = new RoomCodeEditorChangeEventHandler.Request(current.RoomId, codeEditorPayload.Enabled, EVRoomCodeEditorChangeSource.System)
+        {
+            SaveChanges = false,
+        };
+        await _roomCodeEditorChangeEventHandler.HandleAsync(request, cancellationToken);
+    }
+
+    private async Task ChangeCodeEditorContentAsync(RoomQuestion current, CancellationToken cancellationToken)
+    {
+        var codeEditorContent = await GetCodeEditorContentAsync(current, cancellationToken);
+        var upsertCodeStateRequest = new UpsertCodeStateRequest
+        {
+            RoomId = current.RoomId,
+            CodeEditorContent = codeEditorContent ?? string.Empty,
+            ChangeCodeEditorContentSource = EVRoomCodeEditorChangeSource.System,
+            SaveChanges = false,
+        };
+        await _roomConfigurationRepository.UpsertCodeStateAsync(upsertCodeStateRequest, cancellationToken);
+    }
+
+    private ValueTask<string?> GetCodeEditorContentAsync(RoomQuestion current, CancellationToken cancellationToken)
+    {
+        if (current.Question is not null && current.Question.CodeEditorId is null)
+        {
+            return ValueTask.FromResult<string?>(null);
+        }
+
+        var content = current.Question?.CodeEditor?.Content;
+        if (content is not null)
+        {
+            return ValueTask.FromResult<string?>(content);
+        }
+
+        var task = _db.Questions
+            .AsNoTracking()
+            .Include(e => e.CodeEditor)
+            .Where(e => e.Id == current.QuestionId)
+            .Select(e => e.CodeEditor == null ? null : e.CodeEditor.Content)
+            .FirstOrDefaultAsync(cancellationToken);
+        return new ValueTask<string?>(task);
     }
 
     private async Task<RoomCodeEditorEnabledEvent.Payload> GetCodeEditorEventPayload(RoomQuestion roomQuestion, CancellationToken cancellationToken)
