99990a92352f8c412764f34f6ffb0563c07500f0
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-12-14 23:35:51 +0300

    simple-peer workaround

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  define: {
-    global: {},
+  resolve: {
+    alias: {
+      'simple-peer': 'simple-peer/simplepeer.min.js',
+    },
   },
   server: {
     proxy: {
