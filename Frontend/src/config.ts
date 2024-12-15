50d93a1811ee070c283df5f903f06080aefb0a5f
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-12-15 19:46:51 +0300

    Rename VITE_NAME to VITE_APP_NAME

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -17,7 +17,7 @@ const getFromEnvOrDefault = (varName: string, defaultValue: string) => {
 export const VITE_BACKEND_URL = getFromEnv('VITE_BACKEND_URL');
 export const VITE_WS_URL = getFromEnv('VITE_WS_URL');
 export const VITE_BUILD_HASH = getFromEnv('VITE_BUILD_HASH');
-export const VITE_NAME = getFromEnvOrDefault('VITE_NAME', 'Interview Platform');
+export const VITE_NAME = getFromEnvOrDefault('VITE_APP_NAME', 'Interview Platform');
 export const VITE_FEEDBACK_IFRAME_URL = getFromEnvOrDefault(
   'VITE_FEEDBACK_IFRAME_URL',
   '',
