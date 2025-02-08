f3f90a5eb46a03e2ca838a3fb6c02cc3e0c245e8
devpav <devpavdeveloper@yandex.ru>
2025-02-07 20:05:05 -0800

    feature/#608 Reformat code command

--- Frontend/src/pages/Room/hooks/useVoiceRecognitionAccum.ts
+++ Frontend/src/pages/Room/hooks/useVoiceRecognitionAccum.ts
@@ -28,7 +28,7 @@ type VoiceRecognitionAccumAction =
     };
 
 const checkIsCommandTranscript = (transcript: string, command: string[]) => {
-  const words = transcript.toLowerCase().replaceAll('.', '').split(' ');
+  const words = transcript.toLowerCase().replaceAll('.', '').replaceAll('ё', 'е').split(' ');
   const firstWordIndex = words.indexOf(command[0]);
   if (firstWordIndex === -1) {
     return false;
