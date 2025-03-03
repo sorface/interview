e3612b15c7a1d41ecd74ebd81349af4504e56bde
VladislavPetyukevich <pet.vladislav@gmail.com>
2025-03-03 18:48:45 +0300

    Frontend: eslint-plugin-react-hooks

--- Frontend/eslint.config.mjs
+++ Frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,6 +11,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooks.configs['recommended-latest'],
   {
     ignores: ['src/setupProxy.js', 'build', 'node_modules', 'coverage'],
   },
