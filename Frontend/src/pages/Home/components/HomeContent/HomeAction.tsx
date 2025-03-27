59cec6318fd8b3f2520e4eb87afd85479b82dbf6
Павел Талайко <devpavdeveloper@yandex.ru>
2025-03-27 07:16:18 +0300

    feature/jwt-auth add logout header

--- Frontend/src/pages/Home/components/HomeContent/HomeAction.tsx
+++ Frontend/src/pages/Home/components/HomeContent/HomeAction.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { VITE_BACKEND_URL } from '../../../../config';
+import {VITE_GATEWAY_URL} from '../../../../config';
 import { LocalizationKey } from '../../../../localization';
 import { useLocalizationCaptions } from '../../../../hooks/useLocalizationCaptions';
 import { Button } from '../../../../components/Button/Button';
@@ -10,7 +10,7 @@ import { Gap } from '../../../../components/Gap/Gap';
 export const HomeAction: FunctionComponent = () => {
   return (
     <a
-      href={`${VITE_BACKEND_URL}/login/sorface?redirectUri=${encodeURIComponent(window.location.href)}`}
+      href={`${VITE_GATEWAY_URL}/oauth2/authorization/passport?redirect-location=${encodeURIComponent(window.location.href)}`}
     >
       <Button variant="invertedActive">
         {useLocalizationCaptions()[LocalizationKey.Login]}
