50a0d6d3be40a5f65dc3151e6f0c7df30b23319f
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2025-05-13 17:11:52 +0400

    closed #723 Backend, api/questions: Added author (#744)
    * Backend, api/questions: Added author

--- Backend/Interview.Domain/Questions/Records/FindPage/QuestionItem.cs
+++ Backend/Interview.Domain/Questions/Records/FindPage/QuestionItem.cs
@@ -12,7 +12,13 @@ public class QuestionItem
     {
         Id = e.Id,
         Value = e.Value,
-        Tags = e.Tags.Select(t => new TagItem { Id = t.Id, Value = t.Value, HexValue = t.HexColor, }).ToList(),
+        Tags = e.Tags.Select(t => new TagItem
+        {
+            Id = t.Id,
+            Value = t.Value,
+            HexValue = t.HexColor,
+        })
+            .ToList(),
         Category = e.Category != null
             ? new CategoryResponse
             {
@@ -37,6 +43,13 @@ public class QuestionItem
                 Content = e.CodeEditor.Content,
                 Lang = e.CodeEditor.Lang,
             },
+        Author = e.CreatedBy == null
+            ? null
+            : new QuestionItemAuthorResponse
+            {
+                UserId = e.CreatedBy.Id,
+                Nickname = e.CreatedBy.Nickname,
+            },
     });
 
     public Guid Id { get; init; }
@@ -50,4 +63,6 @@ public class QuestionItem
     public required QuestionCodeEditorResponse? CodeEditor { get; init; }
 
     public required CategoryResponse? Category { get; init; }
+
+    public required QuestionItemAuthorResponse? Author { get; init; }
 }
