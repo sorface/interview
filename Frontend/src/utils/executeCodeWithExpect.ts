602e266f8052149657863c6b0362722fea83c029
VladislavPetyukevich <pet.vladislav@gmail.com>
2025-05-26 20:52:50 +0300

    Frontend: expectConsole in executeCodeWithExpect

--- Frontend/src/utils/executeCodeWithExpect.ts
+++ Frontend/src/utils/executeCodeWithExpect.ts
@@ -2,7 +2,22 @@ import { AnyObject } from '../types/anyObject';
 import { deepEqual } from './deepEqual';
 
 const expectCode = `
+  return new Promise((res) => {
   const __expectCalls = [];
+  const __consoleLogs = [];
+  let __expectedConsoleLogs = [];
+
+  const originalConsoleLog = console.log;
+  console.log = (...args) => {
+    __consoleLogs.push(args);
+    const currentConsoleLogIndex = __consoleLogs.length - 1;
+    __expectCalls.push([__expectedConsoleLogs[currentConsoleLogIndex], args]);
+    originalConsoleLog(...args);
+    if (__expectedConsoleLogs.length === __consoleLogs.length) {
+      res(__expectCalls);
+    }
+  };
+
   const expect = (fn, ...argsWithExpected) => {
     const args = [...argsWithExpected];
     args.length--;
@@ -19,10 +34,17 @@ const expectCode = `
   const expectValue = (value1, value2) => {
     __expectCalls.push([value1, value2]);
   };
+
+  const expectConsole = (expectedCalls) => {
+    __expectedConsoleLogs = expectedCalls;
+  };
 `;
 
 const expectCallsReturnCode = `
-  return __expectCalls;
+  if (!__expectedConsoleLogs) {
+    res(__expectCalls);
+  }
+});
 `;
 
 type Arg = number | string | AnyObject;
@@ -38,15 +60,16 @@ export type ExpectResult = {
   passed: boolean;
 };
 
-export const executeCodeWithExpect = (
+export const executeCodeWithExpect = async (
   code: string | undefined,
-): ExecuteCodeResult => {
+): Promise<ExecuteCodeResult> => {
   try {
     const executeCodeWithExpect = new Function(
       expectCode + code + expectCallsReturnCode,
-    ) as () => Array<Arg[]>;
+    ) as () => Promise<Array<Arg[]>>;
 
-    const executeResult = executeCodeWithExpect();
+    const executeResult = await executeCodeWithExpect();
+    console.log('executeResult: ', executeResult);
 
     return {
       results: executeResult.map((res) => {
